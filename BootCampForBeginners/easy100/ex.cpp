bd45eb67aa56af63494d973ef0ae5bbb2f2c79f9
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-04-25 21:10:27 +0900

    テンプレート更新

--- BootCampForBeginners/easy100/ex.cpp
+++ BootCampForBeginners/easy100/ex.cpp
@@ -1,6 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
 #define rep(i, x, n) for (int i = x; i < (int)(n); i++)
 
 int main()
