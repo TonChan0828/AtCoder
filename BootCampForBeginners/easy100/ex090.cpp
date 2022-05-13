bdcebdcaeeb01153ba3a79f2492a0977fcccf547
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-05-13 23:45:59 +0900

    add to ex090

---
+++ BootCampForBeginners/easy100/ex090.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); i--)
+
+int main()
+{
+  int n;
+  cin >> n;
+  ll pwr = 1;
+  int limit = pow(10, 9) + 7;
+  rep(i, 1, n + 1)
+  {
+    pwr = (pwr * i) % limit;
+  }
+  cout << pwr << endl;
+  return 0;
+}
