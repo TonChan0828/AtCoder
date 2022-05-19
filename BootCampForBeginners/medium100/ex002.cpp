94c4117506889b702fd93148e40264183659ebaa
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-05-19 23:32:52 +0900

    aadd to ex002

---
+++ BootCampForBeginners/medium100/ex002.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); i--)
+
+int main()
+{
+  string s;
+  cin >> s;
+  ll count = 0;
+  int pos = 0;
+  rep(i, 0, s.size())
+  {
+    if (s[i] == 'W')
+    {
+      count += i - pos;
+      pos++;
+    }
+  }
+  cout << count << endl;
+  return 0;
+}
