65632987036a4411cb374f6823cd00e272b1e7f5
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-06-28 07:16:18 +0900

    add to ex073

---
+++ BootCampForBeginners/medium100/ex073.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); i--)
+
+int main()
+{
+  long double a, b, c;
+  cin >> a >> b >> c;
+  if (sqrt(a) + sqrt(b) + (1.0E-14) < sqrt(c))
+  {
+    cout << "Yes" << endl;
+  }
+  else
+  {
+    cout << "No" << endl;
+  }
+  return 0;
+}
