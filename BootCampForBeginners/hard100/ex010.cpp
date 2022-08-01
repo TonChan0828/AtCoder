e9f51494769253d83bb5d3b4d6c36b049978379a
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-08-01 19:16:36 +0900

    add to ex010

---
+++ BootCampForBeginners/hard100/ex010.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); i--)
+bool myCompare(pair<int, int> a, pair<int, int> b)
+{
+  if (a.first != b.first)
+  {
+    return a.first > b.first;
+  }
+  if (a.second != b.second)
+  {
+    return a.second < b.second;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+int main()
+{
+  int n;
+  ll k;
+  cin >> n >> k;
+  vector<ll> a(n + 1), sum(n + 1, 0);
+
+  rep(i, 1, n + 1)
+  {
+    cin >> a[i];
+    sum[i] = sum[i - 1] + a[i];
+  }
+  ll ans = 0;
+  rep(i, 0, n + 1)
+  {
+    auto it = lower_bound(sum.begin(), sum.end(), k + sum[i]);
+    ans += sum.end() - it;
+  }
+  cout << ans << endl;
+  return 0;
+}
