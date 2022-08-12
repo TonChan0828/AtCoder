f837bb8c7a6733454a472112609fa027afcce116
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-08-12 18:45:55 +0900

    add to ex021

---
+++ BootCampForBeginners/hard100/ex021.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); i--)
+bool myCompare(pair<int, int> a, pair<int, int> b)
+{
+  if (a.first != b.first)
+  {
+    return a.first > b.first;
+  }
+  if (a.second != b.second)
+  {
+    return a.second < b.second;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  vector<ll> a(n);
+  rep(i, 0, n)
+  {
+    cin >> a[i];
+  }
+  sort(a.begin(), a.end());
+  ll r = a[0];
+  rep(i, 0, n - 1)
+  {
+    if (llabs(a[i] - a[n - 1] / 2) <= llabs(r - a[n - 1] / 2))
+    {
+      r = a[i];
+    }
+  }
+  cout << a[n - 1] << " " << r << endl;
+  return 0;
+}
