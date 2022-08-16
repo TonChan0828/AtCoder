c378a39147577fe56656146d1c3c392e8ad68fc1
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-08-17 08:33:15 +0900

    add to ex026

---
+++ BootCampForBeginners/hard100/ex026.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); i--)
+bool myCompare(pair<int, int> a, pair<int, int> b)
+{
+  if (a.first != b.first)
+  {
+    return a.first > b.first;
+  }
+  if (a.second != b.second)
+  {
+    return a.second < b.second;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+int main()
+{
+  int k, t;
+  cin >> k >> t;
+  vector<int> a(t);
+  rep(i, 0, t)
+  {
+    cin >> a[i];
+  }
+  sort(a.begin(), a.end(), greater());
+  vector<int> d(2);
+  rep(i, 0, t)
+  {
+    if (d[0] >= d[1])
+    {
+      d[1] += a[i];
+    }
+    else
+    {
+      d[0] += a[i];
+    }
+  }
+  int ans = 0;
+  if (abs(d[0] - d[1]) > 1)
+  {
+    ans = abs(d[0] - d[1]) - 1;
+  }
+  cout << ans << endl;
+  return 0;
+}
