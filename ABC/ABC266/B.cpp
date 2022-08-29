f155cefb91361d5d957a78c69691ff665012d9c8
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-08-29 21:31:44 +0900

    add ABC266 B

---
+++ ABC/ABC266/B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); i--)
+bool myCompare(pair<int, int> a, pair<int, int> b)
+{
+  if (a.first != b.first)
+  {
+    return a.first > b.first;
+  }
+  if (a.second != b.second)
+  {
+    return a.second < b.second;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+int main()
+{
+  ll n;
+  cin >> n;
+  ll x = 998244353;
+  ll ans;
+  if (n % x < 0)
+  {
+    ans = x + n % x;
+  }
+  else
+  {
+    ans = n % x;
+  }
+  cout << ans << endl;
+  return 0;
+}
