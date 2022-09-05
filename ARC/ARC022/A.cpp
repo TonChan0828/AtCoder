1d7c6de8e3f6ec67dc66f39cdb8be607c3d9a29b
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-09-05 21:38:06 +0900

    add ARC022 A

---
+++ ARC/ARC022/A.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); i--)
+bool myCompare(pair<int, int> a, pair<int, int> b)
+{
+  if (a.first != b.first)
+  {
+    return a.first > b.first;
+  }
+  if (a.second != b.second)
+  {
+    return a.second < b.second;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+int main()
+{
+  string s;
+  cin >> s;
+  string ans1 = "ict", ans2 = "ICT";
+  int pos = 0;
+  rep(i, 0, s.size())
+  {
+    if (s[i] == ans1[pos] || s[i] == ans2[pos])
+    {
+      pos++;
+    }
+  }
+  if (pos == 3)
+  {
+    cout << "YES" << endl;
+  }
+  else
+  {
+    cout << "NO" << endl;
+  }
+  return 0;
+}
