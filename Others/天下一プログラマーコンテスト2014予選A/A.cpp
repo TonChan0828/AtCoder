f67a202902aed97932587875ed3cfaa8453e4753
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-09-06 07:29:51 +0900

    add 天下一プログラマーコンテスト2014予選A A

---
+++ Others/天下一プログラマーコンテスト2014予選A/A.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); i--)
+bool myCompare(pair<int, int> a, pair<int, int> b)
+{
+  if (a.first != b.first)
+  {
+    return a.first > b.first;
+  }
+  if (a.second != b.second)
+  {
+    return a.second < b.second;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+int main()
+{
+  const int target = 1001;
+  vector<string> ans;
+  rep(i, 1, target)
+  {
+    ans.push_back(to_string(i));
+  }
+  sort(ans.begin(), ans.end());
+  rep(i, 0, 1000)
+  {
+    cout << ans[i] << endl;
+  }
+  return 0;
+}
