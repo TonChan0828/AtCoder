a518f45515a647ae40f1386ce7e391d8cc0024c1
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-09-16 11:52:22 +0900

    add ex052

---
+++ BootCampForBeginners/3_hard100/ex052.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); i--)
+bool myCompare(pair<int, int> a, pair<int, int> b)
+{
+  if (a.first != b.first)
+  {
+    return a.first > b.first;
+  }
+  if (a.second != b.second)
+  {
+    return a.second < b.second;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+int main()
+{
+  int n, m;
+  cin >> n >> m;
+  int res = 1;
+  for (int i = 1; i * i <= m; i++)
+  {
+    if (m % i != 0)
+    {
+      continue;
+    }
+    int j = m / i;
+    if ((ll)i * n <= m)
+    {
+      res = max(res, i);
+    }
+    if ((ll)j * n <= m)
+    {
+      res = max(res, j);
+    }
+  }
+  cout << res << endl;
+  return 0;
+}
