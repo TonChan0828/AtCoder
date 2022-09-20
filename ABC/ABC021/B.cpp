39286419101b71cbcfcfb6260ad9c58ed1e145ee
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-09-20 22:33:27 +0900

    add ABC021 B

---
+++ ABC/ABC021/B.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); i--)
+bool myCompare(pair<int, int> a, pair<int, int> b)
+{
+  if (a.first != b.first)
+  {
+    return a.first > b.first;
+  }
+  if (a.second != b.second)
+  {
+    return a.second < b.second;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  map<int, int> m;
+  int a, b;
+  cin >> a >> b;
+  m[a]++;
+  m[b]++;
+  int k;
+  cin >> k;
+  rep(i, 0, k)
+  {
+    int p;
+    cin >> p;
+    m[p]++;
+  }
+
+  for (const auto &[key, value] : m)
+  {
+    if (value != 1)
+    {
+      cout << "NO" << endl;
+      return 0;
+    }
+  }
+  cout << "YES" << endl;
+  return 0;
+}
