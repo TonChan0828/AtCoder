61cb9d7dd4a16a32deb6f4793fdd2c388247e1ce
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-09-24 15:49:22 +0900

    add ABC011 A

---
+++ ABC/ABC011/A.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); i--)
+bool myCompare(pair<int, int> a, pair<int, int> b)
+{
+  if (a.first != b.first)
+  {
+    return a.first > b.first;
+  }
+  if (a.second != b.second)
+  {
+    return a.second < b.second;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+int main()
+{
+  int m, n, N;
+  cin >> m >> n >> N;
+  ll ans = N;
+  int r = N;
+  while (r >= m)
+  {
+    int t = (r / m) * n;
+    ans += t;
+    r = r % m + t;
+    // cout << r << endl;
+  }
+  cout << ans << endl;
+  return 0;
+}
