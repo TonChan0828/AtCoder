44999a2d090249ffb27af14703af7e46df31ff85
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-09-26 08:17:53 +0900

    add ABC115 C

---
+++ ABC/ABC115/C.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+  if (a < b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+  if (a > b)
+  {
+    a = b; // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); i--)
+bool myCompare(pair<int, int> a, pair<int, int> b)
+{
+  if (a.first != b.first)
+  {
+    return a.first > b.first;
+  }
+  if (a.second != b.second)
+  {
+    return a.second < b.second;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  vector<int> ans(n + 1);
+  int pos = 1;
+  rep(i, 1, n + 1)
+  {
+    ans[i] = ans[i / 2] + 1;
+  }
+
+  rep(i, 1, n + 1)
+  {
+    if (i != 1)
+    {
+      cout << " ";
+    }
+    cout << ans[i];
+  }
+  cout << endl;
+  return 0;
+}
