2b4471ee498fc650234bde5faf6c2210212b8200
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-10-05 21:59:23 +0900

    add ABC193 C

---
+++ ABC/ABC193/C.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  ll n;
+  cin >> n;
+  unordered_set<ll> us;
+  for (ll i = 2; i * i <= n; ++i) {
+    ll x = i * i;
+    while (x <= n) {
+      us.insert(x);
+      x *= i;
+    }
+  }
+  cout << n - us.size() << endl;
+  return 0;
+}
