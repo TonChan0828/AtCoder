86e3df3fd19de5579e202967d52456f71131c4f0
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-10-09 11:53:44 +0900

    add ABC227 C

---
+++ ABC/ABC227/C.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  ll n;
+  cin >> n;
+  unsigned long long ans = 0;
+  ll i = 1;
+  while (i <= n / i) {
+    ll j = i;
+    ll tn = n / i;
+    while (j <= tn / j) {
+      ans += tn / j - j + 1;
+      // cout << i << " " << j << " " << tn / j << endl;
+      ;
+      ++j;
+    }
+    ++i;
+  }
+  cout << ans << endl;
+  return 0;
+}
