3b7333330336616a4aa6736b6d086603a3efa191
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-10-12 07:20:39 +0900

    add ABC003 A

---
+++ ABC/ABC003/A.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  double n;
+  cin >> n;
+  double sum = 0;
+  rep(i, 0, n) {
+    char c;
+    cin >> c;
+    if (c == 'A') {
+      sum += 4;
+    } else if (c == 'B') {
+      sum += 3;
+    } else if (c == 'C') {
+      sum += 2;
+    } else if (c == 'D') {
+      sum += 1;
+    }
+  }
+  double ans = sum / n;
+  cout << setprecision(12);
+  cout << ans << endl;
+  return 0;
+}
