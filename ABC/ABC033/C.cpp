1fc3f2103753994c020918b351f4478375ec43c1
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-10-12 21:53:15 +0900

    add ABC033 C

---
+++ ABC/ABC033/C.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  string s;
+  cin >> s;
+  int ans = 0;
+  stack<ll> pool;
+  rep(i, 0, s.size()) {
+    if (s[i] == '+') {
+      continue;
+    } else if (s[i] == '*') {
+      ll tmp = pool.top();
+      pool.pop();
+      if (tmp > 0) tmp = 1;
+      tmp *= (ll)(s[++i] - '0');
+      pool.push(tmp);
+    } else {
+      pool.push((ll)(s[i] - '0'));
+    }
+  }
+  while (!pool.empty()) {
+    ll tmp = pool.top();
+    pool.pop();
+    if (tmp != 0) {
+      // cout << tmp << endl;
+      ans++;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
