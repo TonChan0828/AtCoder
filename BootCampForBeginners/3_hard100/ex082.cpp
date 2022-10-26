34c406339b04bcea28361a1c05ac1d28b0898166
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-10-26 21:44:42 +0900

    add ex082

---
+++ BootCampForBeginners/3_hard100/ex082.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<pair<pair<ll, ll>, int>> ac(n);
+  vector<ll> sum(n);
+
+  ll ans = 0;
+  rep(i, 0, n) {
+    ll a, b;
+    cin >> a >> b;
+    ans -= b;
+    sum[i] = a + b;
+    ac[i] = {{a, b}, i};
+  }
+  sort(sum.begin(), sum.end(), greater());
+  rep(i, 0, n) {
+    if (i % 2 == 0) {
+      ans += sum[i];
+    }
+  }
+
+  cout << ans << endl;
+  return 0;
+}
