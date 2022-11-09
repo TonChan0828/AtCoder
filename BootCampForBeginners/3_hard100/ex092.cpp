a96a419f73ff1b36862f65aba1b5eec683c77d05
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-11-09 21:50:00 +0900

    add ex092

---
+++ BootCampForBeginners/3_hard100/ex092.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<vector<int>> t(n + 1);
+  rep(i, 0, m) {
+    int a, b;
+    cin >> a >> b;
+    t[a].push_back(b);
+    t[b].push_back(a);
+  }
+  rep(i, 0, n) {
+    if (t[i].size() % 2 != 0) {
+      cout << "NO" << endl;
+      return 0;
+    }
+  }
+  cout << "YES" << endl;
+  return 0;
+}
