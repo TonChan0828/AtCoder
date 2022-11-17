168c555353bb8a1f9a3a4a60c1789d69089110c2
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-11-17 23:30:37 +0900

    add ABC009 A

---
+++ BootCampForBeginners/3_hard100/ex097.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  string ans;
+  while (n != 0) {
+    if (n % 2 != 0) {
+      --n;
+      ans = '1' + ans;
+    } else {
+      ans = '0' + ans;
+    }
+    n /= -2;
+  }
+  if (ans == "") ans = '0';
+  cout << ans << endl;
+  return 0;
+}
