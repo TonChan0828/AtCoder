efa3e4eadf8cab734ae9742983c88b1efb948963
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-12-04 11:58:25 +0900

    add EDPC D

---
+++ Others/EDPC/D.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int N, W;
+  cin >> N >> W;
+  vector<ll> w(N + 1), v(N + 1);
+  rep(i, 1, N + 1) cin >> w[i] >> v[i];
+  vector<vector<ll>> dp(N + 1, vector<ll>(W + 1, 0));
+  rep(i, 1, N + 1) {
+    rep(j, 0, W + 1) {
+      if (j < w[i]) {
+        dp[i][j] = dp[i - 1][j];
+      } else {
+        dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+      }
+    }
+  }
+  cout << dp[N][W] << endl;
+  return 0;
+}
