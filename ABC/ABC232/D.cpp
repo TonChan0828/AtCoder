7baf5d5ec0c1b18195d356e689384de040e075ca
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-12-13 22:36:41 +0900

    add ABC232 D

---
+++ ABC/ABC232/D.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int h, w;
+  cin >> h >> w;
+  vector<vector<char>> c(h, vector<char>(w));
+  rep(i, 0, h) { rep(j, 0, w) cin >> c[i][j]; }
+
+  vector<vector<bool>> checked(h, vector<bool>(w, false));
+  auto f = [&](auto f, int y, int x, int cnt) -> int {
+    if (checked[y][x]) return cnt;
+    if (c[y][x] == '#') return cnt;
+    if (x == w - 1 && y == h - 1) return cnt + 1;
+    int ans = cnt;
+    checked[y][x] = true;
+    if (x + 1 < w) ans = max(cnt, f(f, y, x + 1, cnt + 1));
+    if (y + 1 < h) ans = max(ans, f(f, y + 1, x, cnt + 1));
+    // printf("%d %d %d\n", y, x, ans);
+    return ans;
+  };
+  cout << f(f, 0, 0, 0) << endl;
+  return 0;
+}
