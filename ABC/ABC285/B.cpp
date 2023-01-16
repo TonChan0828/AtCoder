6d9d6649fc7710969a87b148b795b5ff2f7d4676
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-01-16 20:49:54 +0900

    add ABC285 A-B

---
+++ ABC/ABC285/B.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  rep(i, 1, n) {
+    int t = 0;
+    while (t + i < n) {
+      if (s[t] != s[t + i]) {
+        t++;
+      } else {
+        break;
+      }
+    }
+    cout << t << endl;
+  }
+  return 0;
+}
