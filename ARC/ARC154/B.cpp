ef27aa104267a96f8e83ea57fd2c65242c13975e
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-01-27 11:59:30 +0900

    add ARC154 B

---
+++ ARC/ARC154/B.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  string s, t;
+  cin >> s >> t;
+  string ts = s, tt = t;
+  sort(ts.begin(), ts.end());
+  sort(tt.begin(), tt.end());
+  if (ts != tt) {
+    cout << -1 << endl;
+    return 0;
+  }
+
+  reverse(s.begin(), s.end());
+  reverse(t.begin(), t.end());
+  int cnt = 0;
+  rep(i, 0, n) {
+    if (s[cnt] == t[i]) ++cnt;
+  }
+  cout << n - cnt << endl;
+  return 0;
+}
