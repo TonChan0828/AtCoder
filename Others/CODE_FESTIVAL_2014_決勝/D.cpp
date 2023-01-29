731799f71c4485dee9057e28e4a1bc51bdecc1ed
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-01-29 12:45:21 +0900

    add CODE_FESTIVAL_2014_決勝 D

---
+++ Others/CODE_FESTIVAL_2014_決勝/D.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+// #include <atcoder/all>
+using namespace std;
+// using namespace atcoder;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int a;
+  cin >> a;
+  printf("%d %d\n", a + 1, 2);
+  return 0;
+}
