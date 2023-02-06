dbe1128b90e634bad07343ba52db9a5acbd42ec1
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-02-06 19:03:21 +0900

    add ABC015 A

---
+++ ABC/ABC015/A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+// #include <atcoder/all>
+using namespace std;
+// using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  string a, b;
+  cin >> a >> b;
+  if (a.size() > b.size()) {
+    cout << a << endl;
+  } else {
+    cout << b << endl;
+  }
+  return 0;
+}
