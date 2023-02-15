f466f2b5f5494b9a51a83a3a051163b4f6e5e80e
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-02-15 18:51:55 +0900

    edit template

--- template.cpp
+++ template.cpp
@@ -6,45 +6,35 @@ using namespace std;
 using ll = long long;
 
 template <typename T>
-bool chmax(T &a, const T &b)
-{
-  if (a < b)
-  {
-    a = b; // aをbで更新
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
     return true;
   }
   return false;
 }
 template <typename T>
-bool chmin(T &a, const T &b)
-{
-  if (a > b)
-  {
-    a = b; // aをbで更新
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
     return true;
   }
   return false;
 }
 #define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
 #define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
-bool myCompare(pair<int, int> a, pair<int, int> b)
-{
-  if (a.first != b.first)
-  {
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
     return a.first > b.first;
   }
-  if (a.second != b.second)
-  {
+  if (a.second != b.second) {
     return a.second < b.second;
-  }
-  else
-  {
+  } else {
     return true;
   }
 }
 
-int main()
-{
+int main(){
 
   return 0;
 }
