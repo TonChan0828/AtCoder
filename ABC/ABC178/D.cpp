e3aa5eef1802fdf1032d2fe3d716b98fc7396ccc
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-02-17 19:24:08 +0900

    add ABC178 D

---
+++ ABC/ABC178/D.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int s;
+  cin >> s;
+  int r = 1000000007;
+  vector<int> a(2010, 0);
+  a[0] = 1, a[1] = a[2] = 0;
+  rep(i, 3, s + 1) { a[i] = (a[i - 1] + a[i - 3]) % r; }
+  cout << a[s] << endl;
+  return 0;
+}
