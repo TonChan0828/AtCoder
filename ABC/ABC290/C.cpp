62cc6e0a269fc9d20916b70c93b27f889a1b6847
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-02-21 18:40:36 +0900

    add ABC290 C

---
+++ ABC/ABC290/C.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  vector<int> a(n);
+  rep(i, 0, n) cin >> a[i];
+
+  vector<int> t(k);
+  sort(a.begin(), a.end());
+  int ans = 0;
+  rep(i, 0, n) {
+    if (a[i] == ans) ++ans;
+  }
+  cout << min(k, ans) << endl;
+  return 0;
+}
