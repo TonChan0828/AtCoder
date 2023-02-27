947fc7421b42f6fbd0176bac4a16a8db3e184ef6
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-02-27 22:50:23 +0900

    add ARC157 A

---
+++ ARC/ARC157/A.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int n, a, b, c, d;
+  cin >> n >> a >> b >> c >> d;
+  bool ok = true;
+
+  if (abs(b - c) > 1) {
+    ok = false;
+  } else if (b == 0 && c == 0 && a > 0 && b > 0) {
+    ok = false;
+  }
+  if (ok) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  return 0;
+}
