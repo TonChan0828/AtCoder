2f8c101aa0323512e09eaa17320d6ef9a7332a06
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-03-04 17:04:34 +0900

    add ABC252 D

--- ABC/ABC252/D.cpp
+++ ABC/ABC252/D.cpp
@@ -1,77 +1,56 @@
 #include <bits/stdc++.h>
+
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
 using ll = long long;
-template <typename T>
-bool chmax(T &a, const T &b)
-{
-  if (a < b)
-  {
-    a = b; // aをbで更新
-    return true;
-  }
-  return false;
-}
-template <typename T>
-bool chmin(T &a, const T &b)
-{
-  if (a > b)
-  {
-    a = b; // aをbで更新
-    return true;
-  }
-  return false;
-}
-#define rep(i, x, n) for (int i = x; i < (int)(n); i++)
-#define rrep(i, a, b) for (int i = a; i >= (int)(b); i--)
 
-int main()
-{
-  int n;
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  ll n;
   cin >> n;
-  vector<int> a(n);
-  map<int, int> num;
-
-  rep(i, 0, n)
-  {
-    cin >> a[i];
-    num[a[i]]++;
+  map<ll, ll> num;
+  rep(i, 0, n) {
+    int a;
+    cin >> a;
+    num[a]++;
   }
-  vector<int> d(1);
-  int count = 0;
-  int sum = 0;
-  for (const auto &[key, value] : num)
-  {
-    if (value > 1)
-    {
-      d.push_back(value);
-      sum += value;
-    }
-    else if (value == 1)
-    {
-      count++;
-    }
+  ll ans = n * (n - 1) * (n - 2) / 6;
+  for (auto x : num) {
+    // cout << x.first << " " << x.second << endl;
+    if (x.second < 2) continue;
+    ans -= (x.second * (x.second - 1) / 2) * (n - x.second);
+    ans -= (x.second * (x.second - 1) * (x.second - 2)) / 6;
+    // cout << ans << endl;
   }
-
-  ll ans = count * (count - 1) * (count - 2);
-  ans += count * sum;
-  rep(i, 0, d.size() - 1)
-  {
-    rep(j, i + 1, d.size())
-    {
-      ans += d[i] * d[j] * count;
-    }
-  }
-  rep(i, 0, d.size() - 2)
-  {
-    rep(j, i + 1, d.size() - 1)
-    {
-      rep(k, k + 1, d.size())
-      {
-        ans += d[i] * d[j] * d[k];
-      }
-    }
-  }
-  cout
-      << ans << endl;
+  cout << ans << endl;
   return 0;
 }
