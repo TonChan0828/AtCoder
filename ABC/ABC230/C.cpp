1582c7aae757e40f10a8ff83639bf6fadce52035
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-03-09 19:40:54 +0900

    add ABC230 C

---
+++ ABC/ABC230/C.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  ll n, a, b, p, q, r, s;
+  cin >> n >> a >> b >> p >> q >> r >> s;
+
+  for (ll i = p; i <= q; ++i) {
+    for (ll j = r; j <= s; ++j) {
+      if (llabs(i - a) == llabs(j - b)) {
+        cout << "#";
+      } else {
+        cout << ".";
+      }
+    }
+    cout << endl;
+  }
+  return 0;
+}
