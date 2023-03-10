b864e49c0f8e5f9de3281ddf1d2b60977dd2f83c
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-03-10 18:55:08 +0900

    add CODE_FESTIVAL_2014_Easy A

---
+++ Others/CODE_FESTIVAL_2014_Easy/A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<double> a(n);
+  rep(i, 0, n) cin >> a[i];
+  double sum = 0;
+  rep(i, 1, n) { sum += a[i] - a[i - 1]; }
+  printf("%.12lf\n", sum / (n - 1));
+  return 0;
+}
