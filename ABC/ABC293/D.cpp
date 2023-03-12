72d7c7b4f9e1df44ec1e42784c0daf9e866b6d2f
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-03-12 11:21:56 +0900

    edit ABC293 D

--- ABC/ABC293/D.cpp
+++ ABC/ABC293/D.cpp
@@ -34,48 +34,67 @@ bool myCompare(pair<int, int> a, pair<int, int> b) {
   }
 }
 
+// int main() {
+//   int n, m;
+//   cin >> n >> m;
+//   vector<vector<int>> g(n);
+//   vector<int> deg(n);
+//   rep(i, 0, m) {
+//     int a, c;
+//     char b, d;
+//     cin >> a >> b >> c >> d;
+//     --a, --c;
+//     g[a].push_back(c);
+//     g[c].push_back(a);
+//     ++deg[a];
+//     ++deg[c];
+//   }
+//   vector<bool> vis(n, false);
+//   int x = 0, y = 0;
+
+//   rep(i, 0, n) {
+//     if (vis[i]) continue;
+
+//     queue<int> q;
+//     vis[i] = true;
+//     q.push(i);
+//     bool l = true;
+//     while (!q.empty()) {
+//       int t = q.front();
+//       q.pop();
+//       if (deg[t] != 2) l = false;
+//       for (int v : g[t]) {
+//         if (vis[v]) continue;
+//         q.push(v);
+//         vis[v] = true;
+//       }
+//     }
+
+//     if (l)
+//       ++x;
+//     else
+//       ++y;
+//   }
+
+//   cout << x << " " << y << endl;
+//   return 0;
+// }
+
 int main() {
   int n, m;
   cin >> n >> m;
-  vector<vector<int>> g(n);
-  vector<int> deg(n);
+  dsu uf(n);
+  int x = 0;
   rep(i, 0, m) {
     int a, c;
     char b, d;
     cin >> a >> b >> c >> d;
     --a, --c;
-    g[a].push_back(c);
-    g[c].push_back(a);
-    ++deg[a];
-    ++deg[c];
-  }
-  vector<bool> vis(n, false);
-  int x = 0, y = 0;
-
-  rep(i, 0, n) {
-    if (vis[i]) continue;
-
-    queue<int> q;
-    vis[i] = true;
-    q.push(i);
-    bool l = true;
-    while (!q.empty()) {
-      int t = q.front();
-      q.pop();
-      if (deg[t] != 2) l = false;
-      for (int v : g[t]) {
-        if (vis[v]) continue;
-        q.push(v);
-        vis[v] = true;
-      }
-    }
-
-    if (l)
+    if (uf.same(a, c))
       ++x;
     else
-      ++y;
+      uf.merge(a, c);
   }
-
+  int y = n - m;
   cout << x << " " << y << endl;
-  return 0;
 }
