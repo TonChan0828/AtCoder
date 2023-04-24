cb049d729c17c445d89b7ceb893609139b477c09
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-04-24 19:19:45 +0900

    add ABC004 C

---
+++ ABC/ABC004/C.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  n %= 30;
+
+  vector<int> a(6);
+  rep(i, 0, 6) a[i] = i + 1;
+  rep(i, 0, n) { swap(a[i % 5], a[i % 5 + 1]); }
+  rep(i, 0, 6) cout << a[i];
+  cout << endl;
+  return 0;
+}
