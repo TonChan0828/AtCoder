d3227afd2249e53eeb653edbf7684e63cc71838f
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-04-27 19:20:16 +0900

    add ABC003 C

---
+++ ABC/ABC003/C.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  vector<double> r(n);
+  rep(i, 0, n) cin >> r[i];
+  sort(r.begin(), r.end());
+  double c = 0;
+  rep(i, n - k, n) { c = (c + r[i]) / 2; }
+  cout << setprecision(12);
+  cout << c << endl;
+  return 0;
+}
