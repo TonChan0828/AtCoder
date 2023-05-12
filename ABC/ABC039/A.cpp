f6ea477f56f9e3d6d0d0837c8dd06e0e3a2a1a03
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-05-12 19:19:42 +0900

    add ABC039 A

---
+++ ABC/ABC039/A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  int ans = 2 * a * b + 2 * b * c + 2 * c * a;
+  cout << ans << endl;
+  return 0;
+}
