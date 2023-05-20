6bef9af87257f0cdb175fbdcf07fc8ac76518567
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-05-20 16:38:23 +0900

    add ABC006 B

---
+++ ABC/ABC006/B.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<ll> ans(n, 0);
+  ans[0] = 0;
+  ans[1] = 0;
+  ans[2] = 1;
+  rep(i, 3, n) { ans[i] = (ans[i - 1] + ans[i - 2] + ans[i - 3]) % 10007; }
+
+  cout << ans.back() << endl;
+  return 0;
+}
