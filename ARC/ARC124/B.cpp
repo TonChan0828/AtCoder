0c55568a91cbe7f81956369d5ba5607bf7c0c06e
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-05-23 19:09:44 +0900

    add ARC124 B

---
+++ ARC/ARC124/B.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<ll> a(n);
+  map<ll, int> b;
+  rep(i, 0, n) cin >> a[i];
+  rep(i, 0, n) {
+    ll t;
+    cin >> t;
+    ++b[t];
+  }
+  set<ll> ans;
+
+  for (auto [key, value] : b) {
+    bool ok = true;
+    ll tar = a[0] ^ key;
+    auto tmp = b;
+    --tmp[key];
+    rep(i, 1, n) {
+      if (tmp[tar ^ a[i]] > 0) {
+        --tmp[tar ^ a[i]];
+      } else {
+        ok = false;
+      }
+    }
+    if (ok) ans.insert(tar);
+  }
+
+  cout << ans.size() << endl;
+  for (ll x : ans) cout << x << endl;
+  return 0;
+}
