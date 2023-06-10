b6af205994a800c2080b2fd0fac69581272d4c35
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-06-10 10:42:50 +0900

    add ABC266 E

---
+++ ABC/ABC266/E.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+bool myCompare(pair<int, int> a, pair<int, int> b) {
+  if (a.first != b.first) {
+    return a.first > b.first;
+  }
+  if (a.second != b.second) {
+    return a.second < b.second;
+  } else {
+    return true;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  double ans = 0;
+  rep(i, 0, n) {
+    double now = 0;
+    rep(j, 1, 7) { now += max(double(j), ans); }
+    ans = now / 6;
+  }
+  printf("%.10f\n", ans);
+  return 0;
+}
