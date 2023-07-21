f4b10aa89a52faaf2d5d778efb6c29142d074ab7
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-07-21 22:01:08 +0900

    add ABC103 D

---
+++ ABC/ABC103/D.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<pair<int, int>> vp(m);
+  rep(i, 0, m) {
+    int a, b;
+    cin >> a >> b;
+    vp[i] = {b, a};
+  }
+  sort(vp.begin(), vp.end());
+  int ans = 1;
+  int pos = vp[0].first;
+  rep(i, 1, m) {
+    if (pos <= vp[i].second) {
+      ++ans;
+      pos = vp[i].first;
+    }
+  }
+
+  cout << ans << endl;
+  return 0;
+}
