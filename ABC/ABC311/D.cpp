3c411556d654341424776a9e23d2d31134b8cfac
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-07-26 08:38:20 +0900

    edit ABC311 D

--- ABC/ABC311/D.cpp
+++ ABC/ABC311/D.cpp
@@ -1,44 +1,76 @@
 #include <bits/stdc++.h>
 
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
+using ll = long long;
 
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
 #define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
 
-const int dx[] = {0, -1, 0, 1}, dy[] = {-1, 0, 1, 0};
 int main() {
   int n, m;
   cin >> n >> m;
   vector<string> s(n);
   rep(i, 0, n) cin >> s[i];
-  vector vis(n, vector<bool>(m));
-  vector pass(n, vector<bool>(m));
+  vector<vector<bool>> vis(n, vector<bool>(m, false));
+  vector<int> dx{0, -1, 0, 1}, dy{-1, 0, 1, 0};
 
   queue<pair<int, int>> q;
-  q.emplace(1, 1);
-  pass[1][1] = vis[1][1] = true;
+  q.push({1, 1});
+  vis[1][1] = true;
   while (!q.empty()) {
     auto [y, x] = q.front();
     q.pop();
 
     rep(i, 0, 4) {
       int tx = x, ty = y;
-      while (s[ty][tx] == '.') {
-        pass[ty][tx] = true;
+      while (s[ty + dy[i]][tx + dx[i]] != '#') {
+        vis[ty][tx] = true;
         // printf("%d %d\n", ty, tx);
-        ty += dy[i];
-        tx += dx[i];
+        ty += dy[i], tx += dx[i];
       }
-      ty -= dy[i];
-      tx -= dx[i];
       if (vis[ty][tx]) continue;
       vis[ty][tx] = true;
-      q.emplace(ty, tx);
+      q.push({ty, tx});
     }
   }
   int cnt = 0;
   rep(i, 0, n) {
     rep(j, 0, m) {
-      if (pass[i][j]) ++cnt;
+      if (vis[i][j]) ++cnt;
     }
   }
   cout << cnt << endl;
