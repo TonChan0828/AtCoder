91143a8fe15ea37eb863fbec29b72840142383b7
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-07-26 22:42:40 +0900

    add ABC311 E

---
+++ ABC/ABC311/E.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int h, w, n;
+  cin >> h >> w >> n;
+  vector dp(h, vector<int>(w, 1));
+  rep(i, 0, n) {
+    int a, b;
+    cin >> a >> b;
+    --a, --b;
+    dp[a][b] = 0;
+  }
+
+  rep(i, 0, h) {
+    rep(j, 0, w) {
+      if (dp[i][j] == 0) continue;
+      if (!i || !j) continue;
+      dp[i][j] = min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]}) + 1;
+    }
+  }
+
+  ll ans = 0;
+  rep(i, 0, h) { rep(j, 0, w) ans += dp[i][j]; }
+  cout << ans << endl;
+  return 0;
+}
