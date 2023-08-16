cc6dcca7caf29375e73b39a3f975e494cb7eb9e7
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-08-16 17:16:42 +0900

    add ABC242 D

---
+++ ABC/ABC242/D.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  string s;
+  int q;
+  cin >> s >> q;
+  while (q--) {
+    ll t, k;
+    cin >> t >> k;
+    --k;
+    int si = 0;
+    if (t <= 60) {
+      ll b = 1LL << t;
+      si = k / b;
+      k %= b;
+    }
+    ll r = __builtin_popcountll(k);
+    ll l = t - r;
+    ll x = l + r * 2 + (s[si] - 'A');
+    char ans = 'A' + (x % 3);
+    cout << ans << "\n";
+  }
+  return 0;
+}
