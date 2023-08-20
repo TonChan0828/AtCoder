1b8880f615f4973f6a15953f874eba3d1ba4e48c
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-08-21 08:05:39 +0900

    add ABC315 AB

---
+++ ABC/ABC315/B.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> d(n);
+  int sum = 0;
+  rep(i, 0, n) {
+    cin >> d[i];
+    sum += d[i];
+  }
+  int mid = (sum + 1) / 2;
+  sum = 0;
+  rep(i, 0, n) {
+    if (mid - sum <= d[i]) {
+      printf("%d %d\n", i + 1, mid - sum);
+      return 0;
+    }
+    sum += d[i];
+  }
+  return 0;
+}
