c25f6e1ef762a910826314697463b4e55268424f
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-09-08 07:19:25 +0900

    add ABC064 A

---
+++ ABC/ABC064/A.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int r, g, b;
+  cin >> r >> g >> b;
+  int rgb = 100 * r + 10 * g + b;
+  if (rgb % 4 == 0) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+  return 0;
+}
