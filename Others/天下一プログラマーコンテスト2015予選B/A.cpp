a3c3e513e8a53eb6ebd8f602cc6ad0d81edae61d
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-10-19 22:09:50 +0900

    add 天下一プログラマーコンテスト2015予選B A

---
+++ Others/天下一プログラマーコンテスト2015予選B/A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n = 20;
+  vector<ll> a(20);
+  a[0] = a[1] = 100;
+  a[2] = 200;
+  rep(i, 3, n) { a[i] = a[i - 1] + a[i - 2] + a[i - 3]; }
+  cout << a[19] << endl;
+  return 0;
+}
