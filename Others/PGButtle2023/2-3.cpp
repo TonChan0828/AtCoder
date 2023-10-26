6fd156f2122c25d98e6a6b5317cc807fe1ac7b9c
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-10-26 22:38:09 +0900

    edit PGBattle 2-3

--- Others/PGButtle2023/2-3.cpp
+++ Others/PGButtle2023/2-3.cpp
@@ -41,38 +41,33 @@ bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
 }
 using mint = modint998244353;
 
+struct Binomial {
+  vector<mint> fac, invfac, inv;
+  Binomial(int n) : fac(n + 1), invfac(n + 1), inv(n + 1) {
+    fac[0] = invfac[0] = inv[0] = 1;
+    for (int i = 1; i <= n; i++) fac[i] = fac[i - 1] * i;
+    invfac[n] = fac[n].inv();
+    for (int i = n - 1; i >= 0; i--) {
+      invfac[i] = invfac[i + 1] * (i + 1);
+      inv[i + 1] = invfac[i + 1] * fac[i];
+    }
+  }
+} C{303030};
 int main() {
-  int n, k;
-  cin >> n >> k;
-  vector<int> a(n);
-  rep(i, 0, n) cin >> a[i];
-
-  if (k == 0) {
-    cout << 1 << endl;
-    return 0;
-  }
-  vector<mint> kaizyo(n + 1);
-  kaizyo[1] = 1;
-  rep(i, 2, n) { kaizyo[i] = kaizyo[i - 1] * i; }
-
-  vector<vector<int>> ma(k);
-  rep(i, 0, n) { ma[i % k].push_back(a[i]); }
-
-  vector<map<int, int>> vm(k);
-  rep(i, 0, k) {
-    for (int x : ma[i]) {
-      vm[i][x]++;
-    }
-  }
+  int N, K;
+  cin >> N >> K;
+  vector<int> A(N);
+  for (auto &x : A) cin >> x;
   mint ans = 1;
-  rep(i, 0, k) {
-    int sz = ma[i].size();
-    mint tmp = kaizyo[sz];
-    for (auto [key, val] : vm[i]) {
-      tmp /= kaizyo[val];
+  for (int i = 0; i < N; i++) {
+    if (i == K) break;
+    map<int, int> mp;
+    int all = 0;
+    for (int j = i; j < N; j += K) {
+      mp[A[j]]++, all++;
     }
-    ans *= tmp;
+    ans *= C.fac[all];
+    for (auto &[_, val] : mp) ans *= C.invfac[val];
   }
-  cout << ans.val() << endl;
-  return 0;
+  cout << ans.val() << "\n";
 }
