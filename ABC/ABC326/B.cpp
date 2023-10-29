a292a38560cf9cbe58a6b7a0e7607e2fcd3381b2
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-10-29 16:30:08 +0900

    add ABC326 A-C

---
+++ ABC/ABC326/B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  rep(i, n, 1000) {
+    int x = i;
+    int a = x % 10;
+    x /= 10;
+    int b = x % 10;
+    int c = x / 10;
+    if (b * c == a) {
+      cout << i << endl;
+      return 0;
+    }
+  }
+  return 0;
+}
