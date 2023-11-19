914c13939164f15c98e83b274cb365631be85e8a
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-11-20 08:03:41 +0900

    add ABC329 C

---
+++ ABC/ABC329/C.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  map<char, int> mp;
+  char now = s[0];
+  int cnt = 1;
+  rep(i, 1, n) {
+    if (now != s[i]) {
+      mp[now] = max(mp[now], cnt);
+      now = s[i];
+      cnt = 1;
+    } else {
+      ++cnt;
+    }
+  }
+  mp[now] = max(mp[now], cnt);
+  int ans = 0;
+  for (auto [_, val] : mp) ans += val;
+  cout << ans << endl;
+  return 0;
+}
