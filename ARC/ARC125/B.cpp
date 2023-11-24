28e726d0e59a20afb02a91bc77b21716985e5486
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-11-24 21:59:03 +0900

    add ARC125 B

---
+++ ARC/ARC125/B.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+using mint = modint998244353;
+
+int main() {
+  ll n;
+  cin >> n;
+  mint ans = 0;
+  for (ll q = 1; q * q <= n; ++q) {
+    if (n / q < q) continue;
+    if (q % 2 == 0) {
+      ans += (n / q) / 2 - (q - 1) / 2;
+    } else {
+      ans += (n / q + 1) / 2 - (q - 1) / 2;
+    }
+  }
+  cout << ans.val() << endl;
+  return 0;
+}
