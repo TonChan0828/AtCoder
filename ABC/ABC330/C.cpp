1108dcd1e7088568541fb434c44dd2441ed6c9f1
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-11-27 08:19:26 +0900

    add ABC330 C

---
+++ ABC/ABC330/C.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  ll d;
+  cin >> d;
+  vector<ll> sq;
+  for (ll i = 0; i * i <= d; ++i) sq.push_back(i * i);
+  ll ans = d;
+  for (ll i : sq) {
+    ll t = d - i;
+    auto itr = lower_bound(begin(sq), end(sq), t);
+    chmin(ans, llabs(t - *itr));
+    --itr;
+    chmin(ans, llabs(t - *itr));
+  }
+  cout << ans << endl;
+  return 0;
+}
