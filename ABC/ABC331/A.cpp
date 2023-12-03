0b1c81179fe66d949c51cb784d00f3e2c8ae237a
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-12-04 08:33:24 +0900

    add ABC331 A

---
+++ ABC/ABC331/A.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int M, D;
+  cin >> M >> D;
+  int y, m, d;
+  cin >> y >> m >> d;
+  ++d;
+  if (m == M && d == D + 1) {
+    ++y;
+    m = 1;
+    d = 1;
+  } else if (d == D + 1) {
+    ++m;
+    d = 1;
+  }
+  printf("%d %d %d\n", y, m, d);
+  return 0;
+}
