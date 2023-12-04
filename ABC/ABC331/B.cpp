4eab79ddbe77d9b2976932733ab54d2f58a8a03d
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-12-04 20:50:27 +0900

    add ABC331 B

---
+++ ABC/ABC331/B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n, s, m, l;
+  cin >> n >> s >> m >> l;
+  int ans = 101010101;
+  rep(i, 0, n / 6 + 2) {
+    rep(j, 0, n / 8 + 2) {
+      rep(k, 0, n / 12 + 2) {
+        if (i * 6 + j * 8 + k * 12 >= n) {
+          chmin(ans, i * s + j * m + k * l);
+        }
+      }
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
