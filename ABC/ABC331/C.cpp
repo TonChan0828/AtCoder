851e5f5618d81c064eb86348ff01591296e867df
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-12-05 08:17:59 +0900

    add ABC311 C

---
+++ ABC/ABC331/C.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<ll> a(n);
+  rep(i, 0, n) cin >> a[i];
+  auto b = a;
+  sort(b.begin(), b.end());
+  vector<ll> sum(n);
+  rrep(i, n - 1, 0) {
+    if (i == n - 1) {
+      sum[i] = b[i];
+    } else {
+      sum[i] = sum[i + 1] + b[i];
+    }
+  }
+
+  rep(i, 0, n) {
+    auto itr = upper_bound(begin(b), end(b), a[i]);
+    int diff = itr - begin(b);
+    if (diff == n) {
+      printf("0 ");
+    } else {
+      printf("%lld ", sum[diff]);
+    }
+  }
+  cout << endl;
+  return 0;
+}
