3571310351deb1ab326e943effc22cdf9a1cc952
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-12-13 14:12:59 +0900

    add ABC332 C

---
+++ ABC/ABC332/C.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n, m;
+  string s;
+  cin >> n >> m >> s;
+  int cnt = 0, logo = 0, ma = 0, lo = 0;
+  rep(i, 0, n) {
+    if (s[i] == '0') {
+      chmax(ma, cnt);
+      cnt = 0;
+      chmax(logo, lo);
+      lo = 0;
+    } else {
+      if (s[i] == '2') ++lo;
+      ++cnt;
+    }
+  }
+  chmax(ma, cnt);
+  chmax(logo, lo);
+  int ans = logo + max(0, ma - logo - m);
+  cout << ans << endl;
+
+  return 0;
+}
