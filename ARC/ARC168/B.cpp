8c13002a90723fcaffefa2f2dd9f1a7063a47624
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-12-23 12:07:23 +0900

    add ARC168 B

---
+++ ARC/ARC168/B.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  rep(i, 0, n) cin >> a[i];
+  map<int, int> cnt;
+  rep(i, 0, n) cnt[a[i]] ^= 1;
+  int mx = -1, x = 0;
+  for (auto [key, val] : cnt) {
+    if (val) {
+      chmax(mx, key);
+      x ^= key;
+    }
+  }
+
+  if (x) {
+    cout << -1 << endl;
+  } else if (mx == -1) {
+    cout << 0 << endl;
+  } else {
+    cout << mx - 1 << endl;
+  }
+  return 0;
+}
