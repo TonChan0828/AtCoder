693a8188d4c9b8c446aa427a8884bf6838d903c2
TonChan0828 <showxillia1028@yahoo.co.jp>
2023-12-26 19:40:23 +0900

    add ABC334 D

---
+++ ABC/ABC334/D.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n, Q;
+  cin >> n >> Q;
+  vector<ll> r(n);
+  rep(i, 0, n) cin >> r[i];
+  sort(begin(r), end(r));
+  vector<ll> cnt(n);
+  cnt[0] = r[0];
+  rep(i, 1, n) { cnt[i] = cnt[i - 1] + r[i]; }
+  while (Q--) {
+    ll q;
+    cin >> q;
+    auto itr = upper_bound(begin(cnt), end(cnt), q);
+    int res = itr - begin(cnt);
+    cout << res << endl;
+  }
+  return 0;
+}
