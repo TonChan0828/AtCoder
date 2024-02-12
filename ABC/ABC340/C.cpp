39f71298fc5f5b15a40fe60fa5b0be5182f69ac3
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-02-12 09:20:21 +0900

    add ABC340 C

---
+++ ABC/ABC340/C.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  ll n;
+  cin >> n;
+  map<ll, ll> mp;
+
+  auto f = [&](auto f, ll x) -> ll {
+    if (x == 1) return 0;
+    if (mp[x]) return mp[x];
+    mp[x] = f(f, x / 2) + f(f, (x + 1) / 2) + x;
+    return mp[x];
+  };
+
+  cout << f(f, n) << endl;
+
+  return 0;
+}
