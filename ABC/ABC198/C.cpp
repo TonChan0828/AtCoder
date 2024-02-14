760c1d8f57cad2867d31ce73141ec06e51e57290
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-02-15 08:24:03 +0900

    add ABC198 C

---
+++ ABC/ABC198/C.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  double r, x, y;
+  cin >> r >> x >> y;
+  double sq = sqrt(x * x + y * y);
+  int ans = 0;
+  if (sq == r) {
+    ans = 1;
+  } else if (sq <= r + r) {
+    ans = 2;
+  } else {
+    ans = ceil(sq / r);
+  }
+  cout << ans << endl;
+  return 0;
+}
