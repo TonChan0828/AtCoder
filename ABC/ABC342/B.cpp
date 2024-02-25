f6556b3474f6ab873cd7f8a02c0214f61d9bb8bc
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-02-25 11:13:20 +0900

    add ABC342 B

---
+++ ABC/ABC342/B.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> line(n);
+  rep(i, 0, n) {
+    int p;
+    cin >> p;
+    --p;
+    line[p] = i;
+  }
+  int q;
+  cin >> q;
+  while (q--) {
+    int a, b;
+    cin >> a >> b;
+    --a, --b;
+    if (line[a] < line[b]) {
+      cout << a + 1;
+    } else {
+      cout << b + 1;
+    }
+    cout << "\n";
+  }
+  return 0;
+}
