b8fb18ba31498e1b49a12d56807613329bdc98d0
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-02-25 11:09:26 +0900

    add ABC342 A

---
+++ ABC/ABC342/A.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  string s;
+  cin >> s;
+  map<char, int> mp;
+
+  for (char c : s) {
+    mp[c]++;
+  }
+  int sz = s.size();
+  char tar;
+  for (auto [key, val] : mp) {
+    if (val == 1) {
+      tar = key;
+    }
+  }
+  rep(i, 0, sz) {
+    if (s[i] == tar) {
+      cout << i + 1 << endl;
+      return 0;
+    }
+  }
+  return 0;
+}
