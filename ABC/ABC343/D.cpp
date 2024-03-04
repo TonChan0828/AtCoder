f74ea63333a02bf41a55834bab303c4caca40129
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-03-05 08:13:40 +0900

    add ABC343 D

---
+++ ABC/ABC343/D.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n, t;
+  cin >> n >> t;
+  vector<ll> a(t), b(t);
+  rep(i, 0, t) {
+    cin >> a[i] >> b[i];
+    --a[i];
+  }
+
+  int cnt = 1;
+  vector<ll> score(n, 0);
+  map<ll, int> mp;
+  mp[0] = n;
+  rep(i, 0, t) {
+    ll aa = score[a[i]];
+    mp[aa]--;
+    if (mp[aa] == 0) cnt--;
+    aa += b[i];
+    if (mp[aa] == 0) cnt++;
+    mp[aa]++;
+    score[a[i]] = aa;
+    cout << cnt << "\n";
+  }
+  return 0;
+}
