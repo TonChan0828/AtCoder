2b4c4e3b8936f1adae4dfca482b37946c6b303a9
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-03-17 09:39:24 +0900

    add ABC345 A

---
+++ ABC/ABC345/A.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+template <typename T>
+bool asc_desc(T &left, T &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+template <typename T>
+bool desc_asc(T &left, T &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  string s;
+  cin >> s;
+  bool ok = true;
+  if (s[0] != '<') ok = false;
+  if (s.back() != '>') ok = false;
+  rep(i, 1, s.size() - 1) {
+    if (s[i] != '=') ok = false;
+  }
+
+  if (ok) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  return 0;
+}
