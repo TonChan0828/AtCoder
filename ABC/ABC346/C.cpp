d3fb35936e5d12db75057306504775bf45721d3f
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-03-26 08:11:18 +0900

    add ABC346 C

---
+++ ABC/ABC346/C.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+template <typename T>
+bool asc_desc(T &left, T &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+template <typename T>
+bool desc_asc(T &left, T &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  ll k;
+  cin >> n >> k;
+  set<ll> s;
+  rep(i, 0, n) {
+    int a;
+    cin >> a;
+    s.insert(a);
+  }
+
+  ll ans = k * (k + 1) / 2;
+  for (ll a : s) {
+    if (a > k) break;
+    ans -= a;
+  }
+
+  cout << ans << endl;
+
+  return 0;
+}
