5ffb9959003dfbadf6abd3039a3a0b6a5f8bba17
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-04-06 20:59:05 +0900

    add ABC164 D

---
+++ ABC/ABC164/D.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+template <typename T>
+bool asc_desc(T &left, T &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+template <typename T>
+bool desc_asc(T &left, T &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  string s;
+  cin >> s;
+  int n = s.size();
+  vector<int> ss;
+  for (char c : s) ss.push_back(c - '0');
+  reverse(begin(ss), end(ss));
+  int x = 1, tot = 0;
+  vector<int> cnt(2019);
+  ll ans = 0;
+  rep(i, 0, n) {
+    cnt[tot]++;
+    tot += ss[i] * x;
+    tot %= 2019;
+    ans += cnt[tot];
+    x = x * 10 % 2019;
+  }
+  cout << ans << endl;
+  return 0;
+}
