188a6a1a5be6e0306d4748723787678520cce995
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-04-07 11:07:58 +0900

    add:ファイル作成用のスクリプトを作成

---
+++ mkfile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -le 1 ]; then
+    echo "フォルダ名が指定されていません"
+else
+    contest_name=$1
+    folder_name=$1$2
+
+    if [ ! -d "${contest_name}/${folder_name}" ]; then
+        mkdir -p "${contest_name}/${folder_name}"
+    fi
+
+    args=("$@")
+    for ((i=3; i<=$#; i++)); do
+        file_name="${contest_name}/${folder_name}/${args[i]}.cpp"
+        cp template.cpp "${file_name}"
+    done
+    fi
