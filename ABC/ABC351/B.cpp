b67edab099592a4ab79b791127785e2650e687d5
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-04-29 07:55:28 +0900

    add ABC351 B AC

---
+++ ABC/ABC351/B.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+template <typename T>
+bool asc_desc(T &left, T &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+template <typename T>
+bool desc_asc(T &left, T &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<string> a(n), b(n);
+  rep(i, 0, n) cin >> a[i];
+  rep(i, 0, n) cin >> b[i];
+  rep(i, 0, n) {
+    rep(j, 0, n) {
+      if (a[i][j] != b[i][j]) {
+        printf("%d %d\n", i + 1, j + 1);
+        return 0;
+      }
+    }
+  }
+  return 0;
+}
