9d7b35aaeaa694ee3f78f90cfea5e017176747ae
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-05-12 09:03:50 +0900

    add ABC353 D AC

---
+++ ABC/ABC353/D.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+template <typename T>
+bool asc_desc(T &left, T &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+template <typename T>
+bool desc_asc(T &left, T &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+const ll mod = 998244353;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<ll> a(n);
+  rep(i, 0, n) cin >> a[i];
+  vector<ll> sum(n);
+  rrep(i, n - 1, 1) sum[i - 1] = (sum[i] + a[i]) % mod;
+  vector<ll> up(n);
+  rrep(i, n - 1, 1) {
+    ll tmp = 1;
+    while (tmp <= a[i]) tmp *= 10LL;
+    up[i - 1] = (up[i] + tmp) % mod;
+  }
+  ll res = 0;
+  rep(i, 0, n - 1) { res = (res + (a[i] * up[i]) % mod + sum[i]) % mod; }
+
+  cout << res << endl;
+  return 0;
+}
