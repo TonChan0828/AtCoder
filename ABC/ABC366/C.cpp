c5a92b6376005f7f9813201f38c2c4ad6428381b
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-08-13 10:10:50 +0900

    add ABC366 C AC

---
+++ ABC/ABC366/C.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int Q;
+  cin >> Q;
+
+  vector<int> num(1e6 + 1, 0);
+  set<int> s;
+
+  while (Q--) {
+    int q;
+    cin >> q;
+    if (q == 1) {
+      int x;
+      cin >> x;
+      s.insert(x);
+      ++num[x];
+    } else if (q == 2) {
+      int x;
+      cin >> x;
+      --num[x];
+      if (num[x] == 0) s.erase(x);
+    } else {
+      cout << s.size() << "\n";
+    }
+  }
+  return 0;
+}
