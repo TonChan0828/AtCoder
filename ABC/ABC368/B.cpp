21594e467ec321136ee87fad63a777d595f113eb
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-08-26 08:11:27 +0900

    add ABC368 B AC

---
+++ ABC/ABC368/B.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  rep(i, 0, n) cin >> a[i];
+
+  int ans = n;
+  int cnt = 0;
+  while (1) {
+    sort(begin(a), end(a), greater());
+    a[0]--, a[1]--;
+    if (a[0] <= 0) --ans;
+    if (a[1] <= 0) --ans;
+    ++cnt;
+    if (ans <= 1) {
+      cout << cnt << endl;
+      return 0;
+    }
+  }
+  return 0;
+}
