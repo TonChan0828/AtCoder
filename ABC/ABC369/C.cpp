f6ae3720a27bdfadf760a560f6f0facb65af6501
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-09-03 08:00:20 +0900

    add ABC369 C AC

---
+++ ABC/ABC369/C.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  rep(i, 0, n) cin >> a[i];
+
+  vector<int> dist(n);
+  rep(i, 1, n) dist[i] = a[i] - a[i - 1];
+  // rep(i, 0, n) cout << dist[i] << " ";
+  // cout << endl;
+
+  ll ans = n;
+  int l = 0;
+  int now = dist[1];
+  rep(r, 1, n) {
+    if (now == dist[r]) {
+    } else {
+      l = r - 1;
+    }
+    ans += r - l;
+    now = dist[r];
+    // cout << ans << endl;
+  }
+
+  cout << ans << endl;
+  return 0;
+}
