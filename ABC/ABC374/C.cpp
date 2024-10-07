c55e52c70a072365b7efedc27fedc8c85eb91f30
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-10-08 06:59:28 +0900

    add ABC374 C AC

---
+++ ABC/ABC374/C.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> k(n);
+  rep(i, 0, n) cin >> k[i];
+
+  int ans = 1001001001;
+  rep(i, 0, 1 << n) {
+    int a = 0, b = 0;
+    rep(j, 0, n) {
+      if (1 & i >> j) {
+        a += k[j];
+      } else {
+        b += k[j];
+      }
+    }
+    ans = min(ans, max(a, b));
+  }
+
+  cout << ans << endl;
+  return 0;
+}
