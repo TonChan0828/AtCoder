769c3e7d1fd191c43ca9f0420abef571584da8a6
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-10-14 07:45:14 +0900

    add ABC375 B AC

---
+++ ABC/ABC375/B.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  double ans = 0;
+  double nx = 0, ny = 0;
+  rep(i, 0, n) {
+    double x, y;
+    cin >> x >> y;
+    ans += sqrt(abs(x - nx) * abs(x - nx) + abs(y - ny) * abs(y - ny));
+    nx = x, ny = y;
+  }
+  ans += sqrt(abs(0 - nx) * abs(0 - nx) + abs(0 - ny) * abs(0 - ny));
+
+  printf("%.10f\n", ans);
+  return 0;
+}
