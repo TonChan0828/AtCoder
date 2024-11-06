b10394d131b6f0cfad4ec83734735313f2aa3114
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-11-06 19:32:50 +0900

    add ABC168 A AC

---
+++ ABC/ABC168/A.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  n %= 10;
+  if (n == 2 || n == 4 || n == 5 || n == 7 || n == 9) {
+    cout << "hon" << endl;
+  } else if (n == 0 || n == 1 || n == 6 || n == 8) {
+    cout << "pon" << endl;
+  } else {
+    cout << "bon" << endl;
+  }
+  return 0;
+}
