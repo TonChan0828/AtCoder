c4fb261c899dbcc90b316e1357e6ce984e264536
TonChan0828 <showxillia1028@yahoo.co.jp>
2024-12-08 08:41:02 +0900

    add ABC383 A AC

---
+++ ABC/ABC383/A.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> t(n), v(n);
+  rep(i, 0, n) cin >> t[i] >> v[i];
+  int now = 0;
+  int ans = 0;
+  rep(i, 1, t[n - 1] + 1) {
+    if (i < t[now]) {
+      if (ans > 0) --ans;
+    } else {
+      if (ans > 0) --ans;
+      ans += v[now];
+      now++;
+    }
+    // cout << ans << endl;
+  }
+
+  cout << ans << endl;
+  return 0;
+}
