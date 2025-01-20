b4c92767d3f98688fe5d6dfd16fdf7e1c4f3e660
TonChan0828 <showxillia1028@yahoo.co.jp>
2025-01-21 08:09:41 +0900

    add ABC389 C AC

---
+++ ABC/ABC389/C.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<ll> snake;
+  snake.push_back(0);
+  int h = 0, t = 0;
+  rep(i, 0, n) {
+    int q;
+    cin >> q;
+    if (q == 1) {
+      int l;
+      cin >> l;
+      snake.push_back(snake.back() + l);
+      t++;
+    } else if (q == 2) {
+      h++;
+    } else {
+      int k;
+      cin >> k;
+      cout << snake[k - 1 + h] - snake[h] << "\n";
+    }
+  }
+
+  return 0;
+}
