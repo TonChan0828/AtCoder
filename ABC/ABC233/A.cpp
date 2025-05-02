5e835b96483273187ba3186aed996b4f7ab83b35
TonChan0828 <showxillia1028@yahoo.co.jp>
2025-05-02 12:49:27 +0900

    add ABC233 A AC

---
+++ ABC/ABC233/A.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int x, y;
+  cin >> x >> y;
+  int yx = y - x;
+  int ans = 0;
+  if (yx <= 0) {
+    ans = 0;
+  } else {
+    if (yx % 10) yx += 10;
+    ans = yx / 10;
+  }
+  cout << ans << endl;
+  return 0;
+}
