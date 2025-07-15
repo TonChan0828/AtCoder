1ae382544eecfe59efd06f9b77682d654ec07ee2
TonChan0828 <showxillia1028@yahoo.co.jp>
2025-07-15 23:05:37 +0900

    add ABC026 B AC

---
+++ ABC/ABC026/B.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<double> r(n);
+  rep(i, 0, n) cin >> r[i];
+  sort(begin(r), end(r), greater());
+  double ans = 0;
+  rep(i, 0, n) {
+    if (i % 2) {
+      ans -= r[i] * r[i];
+    } else {
+      ans += r[i] * r[i];
+    }
+  }
+  printf("%.7f\n", ans * M_PI);
+  return 0;
+}
