a424dc291d1402bbc91641adf0b375d5340f9f5e
TonChan0828 <showxillia1028@yahoo.co.jp>
2025-07-17 22:10:33 +0900

    add ABC414 D AC

---
+++ ABC/ABC414/D.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<ll> x(n);
+  rep(i, 0, n) cin >> x[i];
+  sort(begin(x), end(x));
+
+  ll ans = x[n - 1] - x[0];
+  priority_queue<ll> q;
+  rep(i, 0, n - 1) { q.push(x[i + 1] - x[i]); }
+  rep(i, 0, m - 1) {
+    ll tmp = q.top();
+    q.pop();
+    ans -= tmp;
+  }
+  cout << ans << endl;
+  return 0;
+}
