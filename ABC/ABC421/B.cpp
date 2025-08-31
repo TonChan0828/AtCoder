8eae57c8a9c15610f778453440f31bb55d16df82
TonChan0828 <showxillia1028@yahoo.co.jp>
2025-09-01 06:56:08 +0900

    add ABC421 B AC

---
+++ ABC/ABC421/B.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  vector<ll> a(10);
+  cin >> a[0] >> a[1];
+
+  rep(i, 2, 10) {
+    int x = a[i - 2] + a[i - 1];
+    string s = to_string(x);
+    reverse(begin(s), end(s));
+    a[i] = stoll(s);
+  }
+
+  cout << a[9] << endl;
+  return 0;
+}
