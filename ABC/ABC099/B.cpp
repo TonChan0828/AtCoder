1bd48f498b5ca5287f740bbe0a79acff818fe70c
TonChan0828 <showxillia1028@yahoo.co.jp>
2025-09-24 18:57:13 +0900

    add ABC099 B AC

---
+++ ABC/ABC099/B.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T &a, const T &b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int> &left, pair<int, int> &right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  int l = 1, r = 1 + 2;
+  rep(i, 3, 1001) {
+    if (l - a > 0 && l - a == r - b) {
+      cout << l - a << endl;
+      break;
+    }
+    l = r;
+    r += i;
+  }
+  return 0;
+}
