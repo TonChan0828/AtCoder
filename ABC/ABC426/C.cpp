266ac483bca13cc7be0734ac73af0070d48e200c
TonChan0828 <showxillia1028@yahoo.co.jp>
2025-10-09 19:24:34 +0900

    add ABC426 C AC

---
+++ ABC/ABC426/C.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+using P = pair<int, int>;
+
+int main() {
+  int n, Q;
+  cin >> n >> Q;
+
+  priority_queue<P, vector<P>, greater<P>> q;
+  rep(i, 1, n + 1) q.push({i, 1});
+
+  while (Q--) {
+    int x, y;
+    cin >> x >> y;
+
+    int cnt = 0;
+    while (1) {
+      auto [num, c] = q.top();
+      if (num > x) break;
+      q.pop();
+      cnt += c;
+    }
+    cout << cnt << "\n";
+    q.push({y, cnt});
+  }
+  return 0;
+}
