b72fa3008332896ac7c714ce4b61fbf2974f4428
TonChan0828 <showxillia1028@yahoo.co.jp>
2025-10-17 06:45:28 +0900

    add ABC111 B AC

---
+++ ABC/ABC111/B.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  vector<int> tar = {111, 222, 333, 444, 555, 666, 777, 888, 999};
+  int x;
+  cin >> x;
+  for (int t : tar) {
+    if (x <= t) {
+      cout << t << endl;
+      return 0;
+    }
+  }
+  return 0;
+}
