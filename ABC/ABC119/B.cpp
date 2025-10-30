7ee298d0958acbde52b77ccc55b6b9c0e29afcf8
TonChan0828 <showxillia1028@yahoo.co.jp>
2025-10-30 22:57:05 +0900

    add ABC119 B AC

---
+++ ABC/ABC119/B.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  double ans = 0;
+  rep(i, 0, n) {
+    double x;
+    string u;
+    cin >> x >> u;
+    if (u == "JPY") {
+      ans += x;
+    } else {
+      ans += x * 380000.0;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
