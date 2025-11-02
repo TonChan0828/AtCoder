1743558cf3e862552a5ce57bbe88bd04a5f0ff15
TonChan0828 <showxillia1028@yahoo.co.jp>
2025-11-03 08:36:53 +0900

    add ABC430 B AC

---
+++ ABC/ABC430/B.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<string> s(n);
+  rep(i, 0, n) cin >> s[i];
+
+  set<vector<string>> ans;
+  rep(i, 0, n - m + 1) {
+    rep(j, 0, n - m + 1) {
+      vector<string> t(m);
+      rep(k, 0, m) {
+        rep(l, 0, m) { t[k] += s[i + k][j + l]; }
+      }
+      ans.insert(t);
+    }
+  }
+
+  cout << (int)ans.size() << endl;
+  return 0;
+}
