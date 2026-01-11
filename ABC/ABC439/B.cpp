67f7c4879e79e9b89be209d71723e4de16d2b711
TonChan0828 <showxillia1028@yahoo.co.jp>
2026-01-11 11:33:50 +0900

    add ABC439 B AC

---
+++ ABC/ABC439/B.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  set<int> s;
+
+  while (s.find(n) == s.end()) {
+    s.insert(n);
+    int res = 0;
+    while (n > 0) {
+      int tmp = n % 10;
+      res += tmp * tmp;
+      n /= 10;
+    }
+    if (res == 1) {
+      cout << "Yes\n";
+      return 0;
+    }
+    n = res;
+  }
+  cout << "No\n";
+  return 0;
+}
