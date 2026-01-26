830a7f3bc235c59570c2ab0740b3c89f7b73823d
TonChan0828 <showxillia1028@yahoo.co.jp>
2026-01-27 07:05:12 +0900

    add ABC442 B AC

---
+++ ABC/ABC442/B.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int q;
+  cin >> q;
+  int v = 0;
+  int s = 0;
+  rep(i, 0, q) {
+    int a;
+    cin >> a;
+    if (a == 1) {
+      v++;
+    } else if (a == 2) {
+      v = max(0, --v);
+    } else {
+      s = (s + 1) % 2;
+    }
+    if (v >= 3 && s) {
+      cout << "Yes\n";
+    } else {
+      cout << "No\n";
+    }
+  }
+
+  return 0;
+}
