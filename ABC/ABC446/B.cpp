6ee17fdd8c0f62b2ad6f2076f48830d628a4c0df
TonChan0828 <showxillia1028@yahoo.co.jp>
2026-03-06 07:20:41 +0900

    add ABC446 B AC

---
+++ ABC/ABC446/B.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  vector<bool> can(m, false);
+  rep(list, 0, n) {
+    int l;
+    cin >> l;
+    vector<int> x(l);
+    rep(i, 0, l) { cin >> x[i], --x[i]; }
+    bool hit = false;
+    rep(i, 0, l) {
+      if (can[x[i]]) continue;
+      can[x[i]] = true;
+      cout << x[i] + 1 << "\n";
+      hit = true;
+      break;
+    }
+    if (!hit) cout << "0\n";
+  }
+
+  return 0;
+}
