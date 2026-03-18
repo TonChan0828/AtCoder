27856f4306050801dc8361ce0c6ea802dd3a9d5b
TonChan0828 <showxillia1028@yahoo.co.jp>
2026-03-19 07:01:34 +0900

    add ABC448 B AC

---
+++ ABC/ABC448/B.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<int> c(m);
+  rep(i, 0, m) cin >> c[i];
+  vector<int> cnt(m);
+  rep(i, 0, n) {
+    int a, b;
+    cin >> a >> b;
+    --a;
+    cnt[a] += b;
+  }
+
+  int ans = 0;
+  rep(i, 0, m) { ans += min(c[i], cnt[i]); }
+  cout << ans << endl;
+  return 0;
+}
