a72c56e473c888d9a08baf4ce029052df3cf606a
TonChan0828 <showxillia1028@yahoo.co.jp>
2026-04-13 09:48:04 +0900

    add ABC453 B AC

---
+++ ABC/ABC453/B.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int t, x;
+  cin >> t >> x;
+  t++;
+
+  vector<int> a(t);
+  rep(i, 0, t) cin >> a[i];
+  vector<pair<int, int>> ans;
+  ans.push_back({0, a[0]});
+  int now = a[0];
+  rep(i, 1, t) {
+    if (abs(now - a[i]) < x) continue;
+    ans.push_back({i, a[i]});
+    now = a[i];
+  }
+
+  for (auto [time, val] : ans) printf("%d %d\n", time, val);
+
+  return 0;
+}
