5024c539a4f43e566605820a911068d76c8872f0
TonChan0828 <showxillia1028@yahoo.co.jp>
2026-05-19 06:22:04 +0900

    add ABC458 B AC

---
+++ ABC/ABC458/B.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+const int di[] = {-1, 0, 0, 1};
+const int dj[] = {0, -1, 1, 0};
+
+int main() {
+  int h, w;
+  cin >> h >> w;
+  rep(i, 0, h) {
+    rep(j, 0, w) {
+      int cnt = 0;
+      rep(k, 0, 4) {
+        int ni = i + di[k], nj = j + dj[k];
+        if (ni < 0 || nj < 0 || ni >= h || nj >= w) continue;
+        ++cnt;
+      }
+      cout << cnt << " ";
+    }
+    cout << "\n";
+  }
+  return 0;
+}
