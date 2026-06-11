5727354d4a66dc7cf82733a1987e8e3ae5a970c7
TonChan0828 <showxillia1028@yahoo.co.jp>
2026-06-12 06:27:42 +0900

    add ABC173 B AC

---
+++ ABC/ABC173/B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n;
+  cin >> n;
+  map<string, int> mp;
+  rep(i, 0, n) {
+    string s;
+    cin >> s;
+    mp[s]++;
+  }
+
+  printf("AC x %d\n", mp["AC"]);
+  printf("WA x %d\n", mp["WA"]);
+  printf("TLE x %d\n", mp["TLE"]);
+  printf("RE x %d\n", mp["RE"]);
+  return 0;
+}
