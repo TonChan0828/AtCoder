1eae1a58a1eede3cb997a0d782ba4172edf999a6
TonChan0828 <showxillia1028@yahoo.co.jp>
2026-06-21 18:16:09 +0900

    add ABC462 D 解析AC

---
+++ ABC/ABC462/D.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+int main() {
+  int n, d;
+  cin >> n >> d;
+  const int m = 1e6 + 5;
+  vector<int> s(m);
+  rep(i, 0, n) {
+    int l, r;
+    cin >> l >> r;
+    // --l;
+    if (r - l < d) continue;
+    r -= d - 1;
+    s[l]++, s[r]--;
+  }
+  rep(i, 0, m - 1) s[i + 1] += s[i];
+
+  ll ans = 0;
+  rep(i, 0, m) {
+    ll x = s[i];
+    ans += x * (x - 1) / 2;
+  }
+  cout << ans << endl;
+  return 0;
+}
