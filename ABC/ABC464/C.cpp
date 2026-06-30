5cc08bf215d672d63cc3dba6dd433ab86d5f7e2e
TonChan0828 <showxillia1028@yahoo.co.jp>
2026-07-01 06:50:43 +0900

    add ABC464 C AC

---
+++ ABC/ABC464/C.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+#define rep(i, x, n) for (int i = x; i < (int)(n); ++i)
+#define rrep(i, a, b) for (int i = a; i >= (int)(b); --i)
+// first昇順 firstが同値の場合second降順
+bool asc_desc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return right.second < left.second;
+  } else {
+    return left.first < right.first;
+  }
+}
+// first降順 firstが同値の場合second昇順
+bool desc_asc(pair<int, int>& left, pair<int, int>& right) {
+  if (left.first == right.first) {
+    return left.second < right.second;
+  } else {
+    return right.first < left.first;
+  }
+}
+
+using P = pair<int, int>;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  vector<int> a(n), d(n), b(n);
+  rep(i, 0, n) cin >> a[i] >> d[i] >> b[i], d[i]--;
+  map<int, int> mp;
+  rep(i, 0, n) mp[a[i]]++;
+  int cnt = mp.size();
+  priority_queue<P, vector<P>, greater<P>> q;
+  rep(i, 0, n) q.emplace(d[i], i);
+
+  rep(mi, 0, m) {
+    while (!q.empty() && mi >= q.top().first) {
+      auto [_, i] = q.top();
+      q.pop();
+      mp[a[i]]--;
+      if (mp[a[i]] == 0) --cnt;
+      mp[b[i]]++;
+      if (mp[b[i]] == 1) ++cnt;
+    }
+    cout << cnt << "\n";
+  }
+  return 0;
+}
